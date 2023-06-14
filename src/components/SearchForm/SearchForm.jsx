0486703e62cf785aa99ef9ce2c89461c29a1e3bd
Kirill Chivchiev <chiefchief.92@mail.ru>
2023-06-14 18:03:46 +0700

    Feature: add formSearch styles

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -4,14 +4,20 @@ import Magnifier from '../../images/search.svg';
 
 function SearchForm() {
   return (
-    <section className="searchForm">
-      <form className="searchForm__formContainer">
-        <fieldset className="searchForm__fieldset">
-          <div className="searchForm__inputContainer">
-            <input type="text" className="searchForm__input" />
-            <buttom className="searchForm__button">
-              <img src={Magnifier} alt="Поиск" className="searchForm__btnImg" />
-            </buttom>
+    <section className="search-form">
+      <form className="searchForm__form-container">
+        <fieldset className="search-form__fieldset">
+          <div className="search-form__inputContainer">
+            <input type="text" placeholder="Фильм" className="search-form__input" />
+            <button type="submit" className="search-form__button">
+              <img src={Magnifier} alt="Поиск" className="search-form__btn-img" />
+            </button>
+          </div>
+          <div className="search-form__checkbox-container">
+            <label className="search-form__switch" htmlFor="checkbox">
+              <input type="checkbox" className="search-form__checkbox" id="checkbox" />
+              <span className="slider circle" />
+            </label>
           </div>
         </fieldset>
       </form>
